b095bb22b558026361387da714e5301f2c21cd46
Roberto Bissanti <roberto@studiobissanti.com>
2026-02-07 16:01:52 +0100

    Intergation wirh obsidian-pandoc plugin

--- obsidian-pandoc-integration/tikz.lua
+++ obsidian-pandoc-integration/tikz.lua
@@ -15,18 +15,29 @@ function CodeBlock(el)
 
   local out = {}
   local caption = nil
+  local shortcaption = nil
   local label = nil
+  local placement = "htbp"
 
   for line in el.text:gmatch("[^\r\n]+") do
     local l = trim(line)
 
-    -- Metadata lines inside tikz block:
-    -- % caption: ...
-    -- % label: ...
+    -- Metadata inside a tikz block:
+    -- % caption: Figure caption text
+    -- % shortcaption: Short caption for list of figures
+    -- % label: fig:my-label
+    -- % placement: htbp / H / ...
     if starts_with(l, "% caption:") then
       caption = trim(l:gsub("^%% caption:", "", 1))
+    elseif starts_with(l, "% shortcaption:") then
+      shortcaption = trim(l:gsub("^%% shortcaption:", "", 1))
     elseif starts_with(l, "% label:") then
       label = trim(l:gsub("^%% label:", "", 1))
+    elseif starts_with(l, "% placement:") then
+      local p = trim(l:gsub("^%% placement:", "", 1))
+      if p ~= "" then
+        placement = p
+      end
     else
       local pkg = l:match("^\\usepackage%b{}")
       if pkg then
@@ -44,13 +55,20 @@ function CodeBlock(el)
 
   if has_tikzpicture and caption and caption ~= "" then
     local fig = {}
-    table.insert(fig, "\\begin{figure}[htbp]")
+    table.insert(fig, "\\begin{figure}[" .. placement .. "]")
     table.insert(fig, "\\centering")
     table.insert(fig, tex)
-    table.insert(fig, "\\caption{" .. caption .. "}")
+
+    if shortcaption and shortcaption ~= "" then
+      table.insert(fig, "\\caption[" .. shortcaption .. "]{" .. caption .. "}")
+    else
+      table.insert(fig, "\\caption{" .. caption .. "}")
+    end
+
     if label and label ~= "" then
       table.insert(fig, "\\label{" .. label .. "}")
     end
+
     table.insert(fig, "\\end{figure}")
     tex = table.concat(fig, "\n")
   elseif has_tikzpicture then
@@ -63,9 +81,11 @@ end
 function Pandoc(doc)
   local includes = doc.meta["header-includes"] or pandoc.List()
   includes:insert(pandoc.RawBlock("latex", "\\usepackage{tikz}"))
+
   for p, _ in pairs(pkgs) do
     includes:insert(pandoc.RawBlock("latex", p))
   end
+
   doc.meta["header-includes"] = includes
   return doc
 end
